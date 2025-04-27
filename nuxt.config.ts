9f877b78632ad904c16a1ebb9910b95573359318
Luki <lks.werner@web.de>
2025-04-27 21:20:14 +0200

    Env setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    'nuxt-security',
+    ['@storyblok/nuxt', { accessToken: 'XVm3HgGZZ3p3kEeNr4pnpAtt' }],
+    '@nuxtjs/tailwindcss'
+  ],
+  devServer: {
+    port: 3001,
+    https: {
+      key: './localhost-key.pem',
+      cert: './localhost.pem'
+    }
+  },
+  security: {
+    headers: {
+      contentSecurityPolicy: {
+        'frame-ancestors': 'app.storyblok.com'
+      },
+    },
+  },
 })
