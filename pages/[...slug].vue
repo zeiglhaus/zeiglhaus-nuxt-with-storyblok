1da31513ead18d9b3cd7a1008bd34f027514a53c
Luki <lks.werner@web.de>
2025-04-27 21:20:29 +0200

    Initial page and components

---
+++ pages/[...slug].vue
@@ -0,0 +1,10 @@
+<script setup>
+const { params } = useRoute();
+const { slug } = params;
+
+const story = await useAsyncStoryblok(slug && slug.length > 0 ? slug.join('/') : 'home', { version: 'draft' })
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
