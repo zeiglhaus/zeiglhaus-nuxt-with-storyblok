d05c955b019cc475bcb09982104975ba649e76a9
Luki <lks.werner@web.de>
2025-05-23 10:33:27 +0200

    Make timeline dots smaller

--- storyblok/TimelineEntry.vue
+++ storyblok/TimelineEntry.vue
@@ -6,7 +6,7 @@ const { blok } = defineProps({ blok: Object })
   <div class="timeline-entry container mx-auto flex px-4">
     <div class="w-0 sm:w-1/2 sm:min-w-1/2 sm:max-w-1/2"/>
     <div class="border-x border-deep-basalt w-0 mx-10 flex-shrink-0">
-      <div class="rounded-full bg-deep-basalt w-6 h-6 -translate-x-1/2 mt-2"/>
+      <div class="rounded-full bg-deep-basalt w-2 h-2 -translate-x-1/2 mt-3.5"/>
     </div>
     <div class="timeline-text-column sm:w-1/2 sm:min-w-1/2 sm:max-w-1/2 mb-10">
       <h2 class="mb-2 text-volcanic-red">{{ blok.year }}</h2>
