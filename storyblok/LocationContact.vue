914e97ff23a58987e380055f9316e1419c992af6
Luki <lks.werner@web.de>
2025-05-26 21:49:57 +0200

    LocationContact img has now rounded class

--- storyblok/LocationContact.vue
+++ storyblok/LocationContact.vue
@@ -11,10 +11,11 @@ const resolvedRichText = computed(() => renderRichText(blok.content))
     v-editable="blok"
 class="container mx-auto my-12 gap-x-4 card grid sm:grid-cols-2">
   <div class="prose p-4" v-html="resolvedRichText"/>
-  <div class="rounded-lg overflow-hidden">
+  <div>
     <img
         :src="blok.map_image?.filename"
         :alt="blok.map_image?.alt"
+        class="rounded-lg"
     >
   </div>
 </div>
