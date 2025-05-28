eb6e70be6530e03f9f36b77b95a0661f1b917008
Luki <lks.werner@web.de>
2025-05-28 20:29:51 +0200

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,8 @@
   <main>
     <Header />
     <slot />
+    <Footer />
   </main>
 </template>
+<script setup lang="ts">
+</script>
---
+++ storyblok/Footer.vue
@@ -0,0 +1,24 @@
+<script setup>
+const storyblokApi = useStoryblokApi();
+
+const footerMenu = ref(null);
+
+const { data } = await storyblokApi.get('cdn/stories/config', {
+  version: 'draft',
+  resolve_links: 'url'
+})
+
+footerMenu.value = data.story.content.footer_links;
+</script>
+<template>
+<div class="p-6">
+  <ul class="flex space-x-8 text-sm justify-center">
+    <li>Zeiglhaus Parkstein e.V.</li>
+    <li v-for="blok in footerMenu" :key="blok._uid">
+      <NuxtLink :to="`/${blok.link.story.url}`" class="hover:text-molten-amber">
+        {{ blok.link.story.name }}
+      </NuxtLink>
+    </li>
+  </ul>
+</div>
+</template>
