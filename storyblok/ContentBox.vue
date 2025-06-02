d8c81dc9fca8ecab72691477202e80a3bfbf740c
Luki <lks.werner@web.de>
2025-06-02 17:24:13 +0200

    LocationContact move to ContentBox

--- storyblok/ContentBox.vue
+++ storyblok/ContentBox.vue
@@ -2,19 +2,17 @@
 const { blok } = defineProps({ blok: Object })
 
 const resolvedRichText = computed(() => renderRichText(blok.content))
-
-
 </script>
 
 <template>
 <div
     v-editable="blok"
-class="container mx-auto my-12 gap-x-4 card grid sm:grid-cols-2">
+class="container mx-auto my-12 card" :class="{'grid gap-x-4 sm:grid-cols-2': blok.image}">
   <div class="prose p-4" v-html="resolvedRichText"/>
-  <div>
+  <div v-if="blok.image">
     <img
-        :src="blok.map_image?.filename"
-        :alt="blok.map_image?.alt"
+        :src="blok.image.filename"
+        :alt="blok.image.alt"
         class="rounded-lg"
     >
   </div>
