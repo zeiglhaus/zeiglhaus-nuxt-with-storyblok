39f132fdd8d1f4a184b9321f3da6df4902718d2a
Luki <lks.werner@web.de>
2025-06-08 19:02:37 +0200

    Overhaul social connect bar

--- storyblok/SocialConnect.vue
+++ storyblok/SocialConnect.vue
@@ -1,38 +1,44 @@
 <script setup>
 const { blok } = defineProps({ blok: Object })
 
+const resolvedRichText = computed(() => renderRichText(blok.subtext))
 
-function colorForIconClass(iconClass) {
-  if (iconClass.includes('whatsapp')) {
-    return 'bg-green-700/60 hover:bg-green-700'
-  } else if (iconClass.includes('instagram')) {
-    return 'bg-pink-900/60 hover:bg-pink-900';
+
+function classForIcon(icon) {
+  if (icon.includes('whatsapp')) {
+    return 'bg-[#25D366] hover:bg-[#1da851]'
+  } else if (icon.includes('instagram')) {
+    return 'bg-gradient-to-r from-[#833AB4] via-[#E1306C] to-[#F77737] hover:saturate-[1.2]';
   } else {
     return 'bg-zh-green';
   }
 }
-
 </script>
+
 <template>
 <div
     v-editable="blok"
-    class="container mx-auto my-8 card flex place-items-center gap-x-4">
-  <Icon name="mdi:information-outline" class="h-20 w-20 bg-black/20 hidden sm:block" />
-  <div class="border-l-4 border-deep-basalt/20 pl-4">
-    <h3 class="mb-4 text-deep-basalt"> {{ blok.title }}</h3>
-    <div class="flex flex-col justify-stretch gap-x-4 gap-y-4 md:flex-row">
-      <NuxtLink
-          v-for="button in blok.buttons" :to="button.link.url"
-          class="flex flex-row flex-grow place-items-center gap-x-2 rounded-xl p-2 text-white"
-          :class="colorForIconClass(button.icon)">
-        <Icon :name="button.icon" class="w-12 h-12" />
-        <div>
-          <h3>{{ button.title }}</h3>
-          <p>{{ button.subtitle }}</p>
-        </div>
-      </NuxtLink>
-    </div>
+    class="container mx-auto my-8 card grid md:grid-cols-2 grid-cols-1 gap-x-4">
+  <div class="p-4">
+    <h2 class="mb-4 text-deep-basalt">{{ blok.title }}</h2>
+    <p v-if="resolvedRichText" class="prose" v-html="resolvedRichText"/>
   </div>
-
+  <div class="flex flex-col justify-center gap-2 sm:gap-4">
+    <NuxtLink
+        v-for="button in blok.buttons"
+        :to="button.link.url"
+        target="_blank"
+        class="group w-full sm:w-auto flex items-center gap-3 px-4 py-2 sm:py-3
+                text-white font-medium rounded-lg
+               transition-all duration-300 ease-in-out hover:shadow-lg hover:scale-[1.02]"
+        :class="classForIcon(button.icon)"
+    >
+      <Icon :name="button.icon" class="flex-shrink-0 w-8 h-8" />
+      <div class="flex flex-col text-left">
+        <span class="text-sm sm:text-base font-bold tracking-wide">{{ button.title }}</span>
+        <span class="text-xs sm:text-sm opacity-90">{{ button.subtitle }}</span>
+      </div>
+    </NuxtLink>
+  </div>
 </div>
 </template>
