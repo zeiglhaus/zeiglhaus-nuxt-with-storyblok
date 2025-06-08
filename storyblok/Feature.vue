3aa84d62fb3ab731b0b105cbe10178509a482518
Luki <lks.werner@web.de>
2025-06-08 20:37:35 +0200

    Change headline color in Feature

--- storyblok/Feature.vue
+++ storyblok/Feature.vue
@@ -12,7 +12,7 @@ const resolvedRichText = computed(() => renderRichText(blok.text))
   >
     <NuxtImg v-if="blok.image" :src="blok.image?.filename" :alt="blok.image?.alt" class="w-full h-48 xl:h-72 object-cover rounded-lg" width="400" height="300" />
     <div>
-      <h3 class="text-2xl text-cooling-lava font-bold mb-2">
+      <h3 class="text-2xl text-volcanic-red font-bold mb-2">
         {{ blok.name }}
       </h3>
       <div v-if="blok.text" class="prose" v-html="resolvedRichText"/>
