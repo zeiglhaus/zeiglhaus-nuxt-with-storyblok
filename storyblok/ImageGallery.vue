f992744ab8c9350d3129a725ca0dc7ffce6f9998
Lukas Werner <lks.werner@web.de>
2025-10-10 19:17:03 +0200

    Add ImageGallery

---
+++ storyblok/ImageGallery.vue
@@ -0,0 +1,27 @@
+<script setup>
+const { blok } = defineProps({ blok: Object })
+</script>
+
+<template>
+  <div
+    v-editable="blok"
+    class="container mx-auto mt-12"
+  >
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 rounded-xl overflow-hidden">
+      <div
+        v-for="(image, index) in blok.images"
+        :key="index"
+      >
+        <NuxtImg
+          v-if="image.filename"
+          :src="image.filename"
+          :alt="image.alt || `Gallery image ${index + 1}`"
+          class="w-full h-64 object-cover hover:scale-105 transition-transform duration-300"
+          width="400"
+          height="300"
+          loading="lazy"
+        />
+      </div>
+    </div>
+  </div>
+</template>
